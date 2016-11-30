7d62f6fd4b512f842a4b79483ab4bb9be88b0969
91076274 <91076274@mvusd.net>
2016-11-30 08:04:18 -0800

    recursion/hanoi/spiderweb/tree

--- section05recursion/SpiderWeb.java
+++ section05recursion/SpiderWeb.java
@@ -1,35 +1,39 @@
 package org.teachingkidsprogramming.section05recursion;
 
 import org.teachingextensions.logo.Tortoise;
+import org.teachingextensions.logo.Turtle.Animals;
+import org.teachingextensions.logo.utils.ColorUtils.PenColors;
 
 public class SpiderWeb
 {
   public static void main(String[] args)
   {
     Tortoise.show();
-    //    Make the tortoise go as fast as possible --#6
-    //    Change the width of the line the tortoise draws to 1 pixel --#12
+    Tortoise.setSpeed(10);
+    Tortoise.setPenWidth(1);
     //    Change the Tortoise to a Spider --#14
-    //    Change the pen color of the line the tortoise draws to red --#13.1
-    //    Set the color of the background window to black (HINT: use the Tortoise to get the background window) --#13.2
+    Tortoise.setAnimal(Animals.Spider);
+    Tortoise.setPenColor(PenColors.Reds.Red);
+    Tortoise.getBackgroundWindow().setBackground(PenColors.Grays.Black);
     Double length = 10.5;
-    //    The current zoom is 1.1 --#8.2
-    //    Do the following 10 times --#10.1
-    //        weaveOneLayer (recipe below) --#9.1
-    //
-    //        ------------- Recipe for weaveOneLayer --#9.2
-    for (int i = 0; i < 6; i++)
+    Double zoom = 1.1;
+    for (int i = 0; i < 10; i++)
     {
       //
-      drawTriangle(length);
+      length = weaveOneLayer(length, zoom);
       //
-      //            Turn the tortoise 1/6th of 360 degrees to the right --#7
-      //            Increase the current length (of the line) by the current zoom --#8.1
+      zoom = zoom * 1.3;
     }
-    //        ------------- End of weaveOneLayer recipe --#9.3
-    //
-    //        Change the current zoom so it is multiplied by 1.3 --#11
-    //    Repeat --#10.2
+  }
+  private static Double weaveOneLayer(Double length, Double zoom)
+  {
+    for (int i = 0; i < 6; i++)
+    {
+      drawTriangle(length);
+      Tortoise.turn(360 / 6);
+      length = length + zoom;
+    }
+    return length;
   }
   private static void drawTriangle(Double length)
   {
